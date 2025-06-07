fa14d83ad8dbb4bec317f9f85d5a74d2f7cb9222
justberare <enattee@gmail.com>
2025-06-07 23:01:29 +0200

    Improve mobile layout

--- settings.php
+++ settings.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Settings</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
